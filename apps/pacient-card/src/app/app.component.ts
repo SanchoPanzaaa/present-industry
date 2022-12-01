ff7f882b002630045253186871965e5a84819bba
dmtsit\rtezbir <rtezbir@techniserv-it.cz>
2022-12-01 16:07:53 +0100

    Initial commit

---
+++ apps/pacient-card/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'present-industry-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'pacient-card';
+}
